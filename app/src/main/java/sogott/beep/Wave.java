861048b19e6e953e3ea81e93c84f5677eae44f07
Snap <XxSnapperGeexX@gmail.com>
2024-10-15 13:34:51 -0700

    Make Wave enum internal.

--- app/src/main/java/sogott/beep/Wave.java
+++ app/src/main/java/sogott/beep/Wave.java
@@ -3,8 +3,8 @@ package sogott.beep;
 import java.util.function.Function;
 import java.util.function.BiFunction;
 
-public enum Wave {
-    SIN((freq, i) -> (sampleRate) -> 2.0 * Math.PI * freq * i / sampleRate);
+enum Wave {
+    SIN((freq, currentSample) -> (sampleRate) -> 2.0 * Math.PI * freq * currentSample / sampleRate);
 
     final private BiFunction<Double, Integer, Function<Float, Double>> _waveTypeFunc;
 
@@ -12,17 +12,17 @@ public enum Wave {
         this._waveTypeFunc = waveTypeFunc;
     }
 
-    final public byte[] generate(double freq, int duration, float sampleRate) {
+    final byte[] generate(double freq, int duration, float sampleRate) {
         final int samples = (int) ((duration / 1000.0) * sampleRate);
         final byte[] output = new byte[samples * 2]; // 16-bit audio
 
-        for (int i = 0; i < samples; ++i) {
-            final double angle = this._waveTypeFunc.apply(freq, i).apply(sampleRate);
+        for (int currentSample = 0; currentSample < samples; ++currentSample) {
+            final double angle = this._waveTypeFunc.apply(freq, currentSample).apply(sampleRate);
             final short sample = (short) (Math.sin(angle) * Short.MAX_VALUE);
 
             // Little endian
-            output[2 * i] = (byte) (sample & 0xFF);
-            output[2 * i + 1] = (byte) ((sample >> 8) & 0xFF);
+            output[2 * currentSample] = (byte) (sample & 0xFF);
+            output[2 * currentSample + 1] = (byte) ((sample >> 8) & 0xFF);
         }
 
         return output;
