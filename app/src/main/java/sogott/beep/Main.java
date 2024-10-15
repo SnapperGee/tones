74ea5c1a2e74f1b8a8cb98f4e70edf1a4b85b286
Snap <XxSnapperGeexX@gmail.com>
2024-10-15 15:34:38 -0700

    Add static Wave square, triangle, and saw methods.

--- app/src/main/java/sogott/beep/Main.java
+++ app/src/main/java/sogott/beep/Main.java
@@ -9,7 +9,7 @@ public class Main {
         final int channels = 1;
         final boolean signed = true;
         final boolean bigEndian = false;
-        final float frequency = 2000;
+        final float frequency = 260;
         final int duration = 750;
 
         final AudioFormat format = new AudioFormat(sampleRate, sampleSizeInBits, channels, signed, bigEndian);
@@ -23,6 +23,9 @@ public class Main {
         byte[] buffer = Wave.sin(frequency, duration);
 
         // Play the waveform
+        line.write(buffer, 0, buffer.length);
+        line.drain();
+        buffer = Wave.triangle(frequency, duration);
         line.write(buffer, 0, buffer.length);
         line.drain();
         line.close();
--- app/src/main/java/sogott/beep/Wave.java
+++ app/src/main/java/sogott/beep/Wave.java
@@ -5,6 +5,14 @@ class Wave {
         return sin(freq, duration, 44100, Short.MAX_VALUE);
     }
 
+    static byte[] square(double freq, int duration) {
+        return square(freq, duration, 44100, Short.MAX_VALUE);
+    }
+
+    static byte[] triangle(double freq, int duration) {
+        return triangle(freq, duration, 44100, Short.MAX_VALUE);
+    }
+
     static byte[] sin(double freq, int duration, float sampleRate, short amplitude) {
         final int samples = (int) ((duration / 1000.0) * sampleRate);
         final byte[] output = new byte[samples * 2]; // 16-bit audio
@@ -19,5 +27,86 @@ class Wave {
         }
 
         return output;
+    }
+
+    static byte[] square(double freq, int duration, float sampleRate, short amplitude) {
+        final int samples = (int) ((duration / 1000.0) * sampleRate);
+        final byte[] output = new byte[samples * 2]; // 16-bit audio
+
+        for (int currentSample = 0; currentSample < samples; ++currentSample) {
+            final double angle = 2.0 * Math.PI * freq * currentSample / sampleRate;
+            final short sample = (short) ((Math.sin(angle) >= 0 ? 1 : -1) * amplitude);
+
+            // Little endian
+            output[2 * currentSample] = (byte) (sample & 0xFF);
+            output[2 * currentSample + 1] = (byte) ((sample >> 8) & 0xFF);
+        }
+
+        return output;
+    }
+
+    static byte[] triangle(double freq, int duration, float sampleRate, short amplitude) {
+        final int samples = (int) ((duration / 1000.0) * sampleRate);
+        final double period = sampleRate / freq;
+        final byte[] output = new byte[samples * 2]; // 16-bit audio
+
+        for (int currentSample = 0; currentSample < samples; ++currentSample) {
+            final double position = currentSample % period;
+            final double value = 2 * Math.abs(2 * (position / period) - 1) - 1;
+            final short sample = (short) (value * amplitude);
+
+            // Little endian
+            output[2 * currentSample] = (byte) (sample & 0xFF);
+            output[2 * currentSample + 1] = (byte) ((sample >> 8) & 0xFF);
+        }
+
+        return output;
+    }
+
+    static class Saw {
+
+        static byte[] up(double freq, int duration) {
+            return up(freq, duration, 44100, Short.MAX_VALUE);
+        }
+
+        static byte[] down(double freq, int duration) {
+            return down(freq, duration, 44100, Short.MAX_VALUE);
+        }
+
+        static byte[] up(double freq, int duration, float sampleRate, short amplitude) {
+            final int samples = (int) ((duration / 1000.0) * sampleRate);
+            final double period = sampleRate / freq;
+            final byte[] output = new byte[samples * 2]; // 16-bit audio
+
+            for (int currentSample = 0; currentSample < samples; ++currentSample) {
+                final double position = currentSample % period;
+                final double value = (2 * (position / period)) - 1;
+                final short sample = (short) (value * amplitude);
+
+                // Little endian
+                output[2 * currentSample] = (byte) (sample & 0xFF);
+                output[2 * currentSample + 1] = (byte) ((sample >> 8) & 0xFF);
+            }
+
+            return output;
+        }
+
+        static byte[] down(double freq, int duration, float sampleRate, short amplitude) {
+            final int samples = (int) ((duration / 1000.0) * sampleRate);
+            final double period = sampleRate / freq;
+            final byte[] output = new byte[samples * 2]; // 16-bit audio
+
+            for (int currentSample = 0; currentSample < samples; ++currentSample) {
+                final double position = currentSample % period;
+                final double value = 1 - (2 * (position / period));
+                final short sample = (short) (value * amplitude);
+
+                // Little endian
+                output[2 * currentSample] = (byte) (sample & 0xFF);
+                output[2 * currentSample + 1] = (byte) ((sample >> 8) & 0xFF);
+            }
+
+            return output;
+        }
     }
 }
