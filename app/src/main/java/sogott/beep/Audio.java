4c088ed458bd3575ec105480ee6e5038f966e011
Snap <XxSnapperGeexX@gmail.com>
2024-10-20 22:24:26 -0700

    Init Audio class.

---
+++ app/src/main/java/sogott/beep/Audio.java
@@ -0,0 +1,83 @@
+package sogott.beep;
+
+import static java.util.Objects.hash;
+
+final class Audio implements Comparable<Audio> {
+    final Wave _wave;
+    final Pitch _pitch;
+    final private int _duration;
+    final private String _string;
+    final private int _hashCode;
+    final private String _toString;
+
+    Audio(Wave wave, Pitch pitch, int duration) {
+        if (wave == null) {
+            throw new IllegalArgumentException(
+                    "Null %s %s.".formatted(Audio.class.getSimpleName(), Wave.class.getSimpleName()));
+        }
+
+        if (pitch == null) {
+            throw new IllegalArgumentException(
+                    "Null %s %s.".formatted(Audio.class.getSimpleName(), Pitch.class.getSimpleName()));
+        }
+
+        this._wave = wave;
+        this._pitch = pitch;
+        this._duration = duration;
+        this._string = "%s>%s.%d".formatted(this._wave.stringValue(), this._pitch.stringValue(), this._duration);
+        this._hashCode = hash(this._wave, this._pitch, this._duration);
+        this._toString = "%s {wave=%s, pitch=\"%s\", duration=%d}".formatted(this._wave.name(),
+                this._pitch.stringValue(), this._duration);
+    }
+
+    Wave wave() {
+        return this._wave;
+    }
+
+    Pitch pitch() {
+        return this._pitch;
+    }
+
+    int duration() {
+        return this._duration;
+    }
+
+    String stringValue() {
+        return this._string;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return obj instanceof Audio other
+                && this._wave == other._wave
+                && this._pitch.equals(other._pitch)
+                && this._duration == other._duration;
+    }
+
+    @Override
+    public int hashCode() {
+        return this._hashCode;
+    }
+
+    @Override
+    public String toString() {
+        return this._toString;
+    }
+
+    @Override
+    public int compareTo(Audio other) {
+        int compareResults = this._wave.compareTo(other._wave);
+
+        if (compareResults != 0) {
+            return compareResults;
+        }
+
+        compareResults = this._pitch.compareTo(other._pitch);
+
+        if (compareResults != 0) {
+            return compareResults;
+        }
+
+        return this._duration - other._duration;
+    }
+}
