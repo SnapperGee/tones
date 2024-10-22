2aa95541bba6ab617944dd4232fda998f0e1bbd4
Snap <XxSnapperGeexX@gmail.com>
2024-10-21 21:07:38 -0700

    Change wave-shape option to wave.

--- app/src/main/java/sogott/beep/CliOption.java
+++ app/src/main/java/sogott/beep/CliOption.java
@@ -5,46 +5,48 @@ import java.util.NoSuchElementException;
 import org.apache.commons.cli.Option;
 
 enum CliOption {
-    BPM(Option.builder("b")
-            .argName("INTEGER")
-            .longOpt("bpm")
-            .hasArg()
-            .desc("Set speed of playback to INTEGER beats per minute (defaults to 140).")
-            .converter(Integer::parseInt)
-            .build()),
-    TIME_SIGNATURE(Option.builder("t")
-            .argName("INTEGER/INTEGER")
-            .longOpt("time-signature")
-            .hasArg()
-            .desc("Set time signature to INTEGER/INTEGER (defaults to 4/4).")
-            .build()),
-    WAVE(Option.builder("w")
-            .argName("WAVE_SHAPE")
-            .longOpt("wave-shape")
-            .hasArg()
-            .converter(aString -> Arrays.stream(Wave.values())
-                    .filter(wave -> wave.stringValueAliases().stream()
-                            .anyMatch(waveStringValue -> aString.equalsIgnoreCase(waveStringValue)))
-                    .findFirst()
-                    .orElseThrow(() -> new NoSuchElementException("Invalid WAVE command line option argument.")))
-            .desc("Set default wave shape to WAVE_SHAPE (defaults to SIN).")
-            .build()),
-    VERSION(Option.builder("v")
-            .longOpt("version")
-            .desc("Prints currently installed version (ignores all other arguments except the help option).")
-            .build()),
-    HELP(Option.builder("h")
-            .longOpt("help")
-            .desc("Ignores all other arguments and prints this help message.")
-            .build());
+        BPM(Option.builder("b")
+                        .argName("INTEGER")
+                        .longOpt("bpm")
+                        .hasArg()
+                        .desc("Set speed of playback to INTEGER beats per minute (defaults to 140).")
+                        .converter(Integer::parseInt)
+                        .build()),
+        TIME_SIGNATURE(Option.builder("t")
+                        .argName("INTEGER/INTEGER")
+                        .longOpt("time-signature")
+                        .hasArg()
+                        .desc("Set time signature to INTEGER/INTEGER (defaults to 4/4).")
+                        .build()),
+        WAVE(Option.builder("w")
+                        .argName("WAVE_SHAPE")
+                        .longOpt("wave")
+                        .hasArg()
+                        .converter(aString -> Arrays.stream(Wave.values())
+                                        .filter(wave -> wave.stringValueAliases().stream()
+                                                        .anyMatch(waveStringValue -> aString
+                                                                        .equalsIgnoreCase(waveStringValue)))
+                                        .findFirst()
+                                        .orElseThrow(() -> new NoSuchElementException(
+                                                        "Invalid WAVE command line option argument.")))
+                        .desc("Set default wave shape to WAVE_SHAPE (defaults to SIN).")
+                        .build()),
+        VERSION(Option.builder("v")
+                        .longOpt("version")
+                        .desc("Prints currently installed version (ignores all other arguments except the help option).")
+                        .build()),
+        HELP(Option.builder("h")
+                        .longOpt("help")
+                        .desc("Ignores all other arguments and prints this help message.")
+                        .build());
 
-    final private Option _value;
+        final private Option _value;
 
-    private CliOption(Option value) {
-        this._value = value;
-    }
+        private CliOption(Option value) {
+                this._value = value;
+        }
 
-    Option value() {
-        return this._value;
-    }
+        Option value() {
+                return this._value;
+        }
 }
