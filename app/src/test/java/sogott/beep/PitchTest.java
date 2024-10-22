f20a0b86b231dc5642362c3db4192c599939d0c1
Snap <XxSnapperGeexX@gmail.com>
2024-10-22 14:54:22 -0700

    Use ArgumentProvider for parameterized tests in PitchTest class.

--- app/src/test/java/sogott/beep/PitchTest.java
+++ app/src/test/java/sogott/beep/PitchTest.java
@@ -3,20 +3,20 @@ package sogott.beep;
 import java.util.stream.Stream;
 import java.util.Arrays;
 import java.util.random.RandomGenerator;
+
+import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-final class PitchTest {
+final class ArgProvider {
     final static RandomGenerator random = RandomGenerator.getDefault();
-
-    private static Stream<Arguments> accidentalAndNonNegativeOctave() {
-        return Arrays.stream(Accidental.values()).map(accidental -> arguments(accidental, random.nextInt(13)));
-    }
 
     private static Stream<Arguments> noteAccidentalAndOctave(int octaveOrigin, int octaveBound) {
         return Arrays.stream(Note.values()).flatMap(note -> Arrays.stream(Accidental.values())
@@ -27,39 +27,73 @@ final class PitchTest {
         return Arrays.stream(Note.values()).map(note -> arguments(note, random.nextInt(octaveOrigin, octaveBound)));
     }
 
-    private static Stream<Arguments> noteAccidentalAndNonNegativeOctave() {
-        return noteAccidentalAndOctave(0, 13);
+    final static class Valid {
+        final static class NoteAccidentalAndOctave implements ArgumentsProvider {
+            @Override
+            public Stream<? extends Arguments> provideArguments(ExtensionContext context) throws Exception {
+                return noteAccidentalAndOctave(0, 13);
+            }
+        }
+
+        final static class NoteAndOctave implements ArgumentsProvider {
+            @Override
+            public Stream<? extends Arguments> provideArguments(ExtensionContext context) throws Exception {
+                return noteAndOctave(0, 13);
+            }
+        }
+
+        final static class AccidentalAndOctave implements ArgumentsProvider {
+            @Override
+            public Stream<? extends Arguments> provideArguments(ExtensionContext context) throws Exception {
+                return Stream.of(
+                        arguments(Accidental.SHARP, random.nextInt(13)),
+                        arguments(Accidental.FLAT, random.nextInt(13)),
+                        arguments(null, random.nextInt(13)));
+            }
+        }
+
+        private Valid() {
+        }
     }
 
-    private static Stream<Arguments> noteAndNonNegativeOctave() {
-        return noteAndOctave(0, 13);
+    final static class Invalid {
+        final static class NoteAccidentalAndNegativeOctave implements ArgumentsProvider {
+            @Override
+            public Stream<? extends Arguments> provideArguments(ExtensionContext context) throws Exception {
+                return noteAccidentalAndOctave(-12, 0);
+            }
+        }
+
+        private Invalid() {
+        }
     }
 
-    private static Stream<Arguments> noteAccidentalAndNegativeOctave() {
-        return noteAccidentalAndOctave(-12, 0);
+    private ArgProvider() {
     }
+}
 
+final class PitchTest {
     @ParameterizedTest(name = "new Pitch(Note.{0}, Accidental.{1}, {2}) does not throw")
-    @MethodSource("noteAccidentalAndNonNegativeOctave")
+    @ArgumentsSource(ArgProvider.Valid.NoteAccidentalAndOctave.class)
     void pitchConstructorWithNoteNonNullAccidentalAndNonNegativeOctaveDoesNotThrow(Note note, Accidental accidental,
             int octave) {
         assertDoesNotThrow(() -> new Pitch(note, accidental, octave));
     }
 
     @ParameterizedTest(name = "new Pitch(Note.{0}, null, {1}) does not throw")
-    @MethodSource("noteAndNonNegativeOctave")
+    @ArgumentsSource(ArgProvider.Valid.NoteAndOctave.class)
     void pitchConstructorWithNoteNullAccidentalAndNonNegativeOctaveDoesNotThrow(Note note, int octave) {
         assertDoesNotThrow(() -> new Pitch(note, null, octave));
     }
 
     @ParameterizedTest(name = "new Pitch(null, Note.{0}, {1}) throws IllegalArgumentException")
-    @MethodSource("accidentalAndNonNegativeOctave")
+    @ArgumentsSource(ArgProvider.Valid.AccidentalAndOctave.class)
     void pitchConstructorWithNullNoteAccidentalAndNonNegativeOctaveThrows(Accidental accidental, int octave) {
         assertThrows(IllegalArgumentException.class, () -> new Pitch(null, accidental, octave));
     }
 
     @ParameterizedTest(name = "new Pitch(Note.{0}, Accidental.{1}, {2}) throws IllegalArgumentException")
-    @MethodSource("noteAccidentalAndNegativeOctave")
+    @ArgumentsSource(ArgProvider.Invalid.NoteAccidentalAndNegativeOctave.class)
     void pitchConstructorWithNoteAccidentalAndNegativeOctaveThrows(Note note, Accidental accidental, int octave) {
         assertThrows(IllegalArgumentException.class, () -> new Pitch(note, accidental, octave));
     }
