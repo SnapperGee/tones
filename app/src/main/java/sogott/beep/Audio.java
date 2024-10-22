016df82875fdbd52e23fa12b0d0902aee84945c1
Snap <XxSnapperGeexX@gmail.com>
2024-10-21 23:05:35 -0700

    Fixup Audio class doc comment.

--- app/src/main/java/sogott/beep/Audio.java
+++ app/src/main/java/sogott/beep/Audio.java
@@ -4,13 +4,16 @@ import static java.util.Objects.hash;
 
 /**
  * The {@link Audio} class contains the properties necessary needed to
- * synthesize audio. The following parts are:
+ * synthesize audio.
+ *
+ * <p>
+ * The following properties are:
  *
  * <ol>
  * <li>
  * <h3><i>Wave</i></h3>
  * The {@code wave} property designates the shape of the wave of the synthesized
- * audio. The following shapes include <i>sine</i>, <i>square</i>,
+ * audio. The following shapes include <i>sin</i>, <i>square</i>,
  * <i>triangle</i>, <i>saw up</i>, and <i>saw down</i>. The wave shapes are
  * defined as the values of the {@link Wave} enum.
  * <li>
@@ -20,9 +23,34 @@ import static java.util.Objects.hash;
  * <li>
  * <h3><i>duration</i></h3>
  * The {@code duration} property designates the duration of the synthesized
- * audio. This is a non negative integer that sets how many milliseconds the
- * audio will play for.
+ * audio. This is a non negative integer that designates the length of the audio
+ * </strong><b>relative to the tempo/bpm and note beat value</b></strong>.
  * </ol>
+ *
+ * An {@link Audio} object can be represented as a {@code String} that can be
+ * divided up into 3 segments, each segment corresponding to each of the 3
+ * properties that an {@link Audio} object is composed of.
+ *
+ * <ol>
+ * <li>The first segment of the string corresponds to the {@link Audio#wave()}
+ * property that defines what the wave shape of the synthesized audio will be. A
+ * bracket/greater than character ({@code '>'}) is then used to delineate it
+ * from the next segment of the string.
+ * <li>The 2nd segment of the string corresponds to the {@link Audio#pitch()}
+ * property that defines the frequency of the synthesized audio. As such, this
+ * segment must correspond to the string format of the {@link Pitch} class. It
+ * must contain at least a note character followed by an octave integer, and can
+ * also contain a plus ({@code '+'}) or minus ({@code '-'}) character after the
+ * note character to indicate whether the note is a <i>flat</i> (&flat;) or
+ * <i>sharp</i> (&sharp;). A period ({@code '.'}) character is then used to
+ * delineate it from the next and final segment of the string.
+ * <li>The final part of the string corresponds to the {@link Audio#duration()}
+ * property and defines the duration of the synthesized audio.
+ * </ol>
+ *
+ * So putting all this together, a {@code String} to explicitly designate a sin
+ * wave that is an F&sharp; (sharp) half note in the 8th octave would be written
+ * as {@code "SIN>F+8.2"}.
  *
  * @author Snap
  * @see Wave
