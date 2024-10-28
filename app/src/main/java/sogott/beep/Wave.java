a83e61ab5a9db1490e4ba9ebce084573ffc1b242
Snap <XxSnapperGeexX@gmail.com>
2024-10-28 13:46:40 -0700

    Add prefixes overload to set case insensitiviy to Wave enum class.

--- app/src/main/java/sogott/beep/Wave.java
+++ app/src/main/java/sogott/beep/Wave.java
@@ -61,10 +61,14 @@ enum Wave {
         return this._stringValueAliases;
     }
 
-    boolean prefixes(String aString) {
+    boolean prefixes(String aString, boolean ignoreCase) {
         return !aString.isEmpty() && this._stringValueAliases.stream()
                 .anyMatch(stringValueAlias -> stringValueAlias.length() >= aString.length()
-                        && aString.regionMatches(true, 0, stringValueAlias, 0, stringValueAlias.length()));
+                        && aString.regionMatches(ignoreCase, 0, stringValueAlias, 0, stringValueAlias.length()));
+    }
+
+    boolean prefixes(String aString) {
+        return prefixes(aString, true);
     }
 
     static Optional<Wave> parse(String aString) {
