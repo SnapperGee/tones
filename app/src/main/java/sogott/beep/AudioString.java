fb0c37c97cde61a4d60ae0e08b77f2bb5d402a53
Snap <XxSnapperGeexX@gmail.com>
2024-10-28 20:11:32 -0700

    Begin updating AudioString class javadoc.

--- app/src/main/java/sogott/beep/AudioString.java
+++ app/src/main/java/sogott/beep/AudioString.java
@@ -4,31 +4,20 @@ import java.util.Arrays;
 import java.util.Optional;
 
 /**
- * A {@code String} can be parsed to audio if it has at least enough
- * information to know the pitch/frequency and duration of the audio. These
- * segments of the string are referred to as the <i>pitch frequency</i> and
- * <i>duration</i> respectively. The duration also being referred to as the
- * <i>duration suffix</i> as it's always the last part of the string. An
- * optional leading prefix can specify the wave shape of the audio. This part is
- * referred to as the <i>wave</i> or <i>wave shape prefix</i> as it's always the
- * leading part of the string if it's present.
- *
- * <p>
- * So an {@link AudioString} can be divided into 3 parts, 1 optional prefix and
- * 2 required parts:
+ * A {@code String} can be parsed to audio if it has the information to define
+ * the <i>wave shape</i>, <i>pitch/frequency</i>, and <i>duration</i> of the
+ * audio. An {@link AudioString} can be divided into 3 segments, each one
+ * defining each of these 3 parts:
  *
  * <ol>
- * <li>Wave shape prefix (optional)
- * <li>* <b>Pitch Frequency</b> (required)
- * <li>* <b>Duration</b> suffix (required)
+ * <li>Wave shape prefix
+ * <li>Pitch Frequency
+ * <li>Duration
  * </ol>
  *
- * {@code "SIN>F+8.2"} is an example of an {@link AudioString} including all
- * optional segments.
- *
  * <h3>1.) Wave shape prefix</h3>
- * The leading segment <i>can</i> specify what kind of wave shape the audio
- * consists of. The types of wave shapes can be designated via:
+ * The leading segment specifies what kind of wave shape the audio consists of.
+ * The types of wave shapes can be designated via:
  *
  * <ul>
  * <li>{@code "SIN"} for a <i>sin</i> wave shape.
@@ -80,11 +69,10 @@ import java.util.Optional;
  * <p>
  * So putting all this together, a {@code String} to explicitly designate a sin
  * wave that is an F&sharp; (sharp) half note in the 8th octave would be
- * {@code "SIN>F+8.2"}. Because a sin wave is the default wave shape, the
- * leading {@code "SIN>"} prefix could have been omitted and it could more
- * concisely be written as {@code "F+8.2"}
+ * {@code "SIN>F+8.2"}.
  *
  * @author Snap
+ * @see Audio
  * @see Wave
  * @see Note
  */
