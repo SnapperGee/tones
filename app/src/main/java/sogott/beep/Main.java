ca16e0aea65a2a254e2515a1c89cc2d59d2041ce
Snap <XxSnapperGeexX@gmail.com>
2024-10-30 21:33:47 -0700

    Delete commented out code.

--- app/src/main/java/sogott/beep/Main.java
+++ app/src/main/java/sogott/beep/Main.java
@@ -148,32 +148,5 @@ final public class Main {
             e.printStackTrace();
             System.err.format("\n%s", e.getMessage());
         }
-
-        // final float sampleRate = 44100;
-        // final int sampleSizeInBits = 16;
-        // final int channels = 1;
-        // final boolean signed = true;
-        // final boolean bigEndian = false;
-        // final float frequency = 260;
-        // final int duration = 750;
-
-        // final AudioFormat format = new AudioFormat(sampleRate, sampleSizeInBits,
-        // channels, signed, bigEndian);
-
-        // // Obtain a line to play the sound
-        // SourceDataLine line = AudioSystem.getSourceDataLine(format);
-        // line.open(format);
-        // line.start();
-
-        // // Generate waveform data (e.g., sine wave)
-        // byte[] buffer = Wave.sin(frequency, duration);
-
-        // // Play the waveform
-        // line.write(buffer, 0, buffer.length);
-        // line.drain();
-        // buffer = Wave.triangle(frequency, duration);
-        // line.write(buffer, 0, buffer.length);
-        // line.drain();
-        // line.close();
     }
 }
