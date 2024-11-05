b0ed45558e2769b09ea8628902e505b4a1557bf0
Snap <XxSnapperGeexX@gmail.com>
2024-11-05 00:15:44 -0800

    Begin adding more tests to AudioString test.

--- app/src/test/java/sogott/beep/AudioStringTest.java
+++ app/src/test/java/sogott/beep/AudioStringTest.java
@@ -1,7 +1,66 @@
 package sogott.beep;
 
+import java.util.stream.Stream;
+import java.util.random.RandomGenerator;
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import static java.util.Collections.unmodifiableList;
+import static java.util.Arrays.asList;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+final class AudioStringArgProvider {
+    private final static List<Wave> waves = unmodifiableList(asList(Wave.values()));
+    private final static List<Note> notes = unmodifiableList(asList(Note.values()));
+    private final static List<Accidental> accidentals = unmodifiableList(asList(Accidental.values()));
+
+    final static RandomGenerator random = RandomGenerator.getDefault();
+
+    static final class Valid {
+        final static class WaveNoteAccidentalAndOctave implements ArgumentsProvider {
+            @Override
+            public Stream<? extends Arguments> provideArguments(ExtensionContext context) throws Exception {
+                return notes.stream().flatMap(note -> {
+                    final int octave = random.nextInt(0, 13);
+                    final int duration = random.nextInt(1, 200000);
+                    return waves.stream()
+                            .flatMap(wave -> wave.stringValueAliases().stream()
+                                    .flatMap(waveStringAlias -> Stream.concat(
+                                            Stream.of(arguments(
+                                                    "%s%c%c%d%c%d".formatted(waveStringAlias,
+                                                            AudioString.Delineator.WAVE_SHAPE_AND_PITCH.charValue(),
+                                                            note.charValue(),
+                                                            octave,
+                                                            AudioString.Delineator.PITCH_AND_DURATION.charValue(),
+                                                            duration),
+                                                    new Audio(wave, new Pitch(note, null, octave), duration))),
+                                            accidentals.stream()
+                                                    .map(accidental -> arguments(
+                                                            "%s%c%c%c%d%c%d".formatted(waveStringAlias,
+                                                                    AudioString.Delineator.WAVE_SHAPE_AND_PITCH
+                                                                            .charValue(),
+                                                                    note.charValue(),
+                                                                    accidental.charValue(),
+                                                                    octave,
+                                                                    AudioString.Delineator.PITCH_AND_DURATION
+                                                                            .charValue(),
+                                                                    duration),
+                                                            new Audio(wave, new Pitch(note, accidental, octave),
+                                                                    duration))))));
+                });
+            }
+        }
+    }
+}
 
 final class AudioStringTest {
     @Test
@@ -17,5 +76,14 @@ final class AudioStringTest {
     @Test
     void AudioStringDelineatorPitchAndDurationCharConstIsPeriod() {
         assertSame('.', AudioString.Delineator.PITCH_AND_DURATION.charValue());
+    }
+
+    @ParameterizedTest(name = "AudioString.parse(\"{0}\") creates optional of {1}")
+    @ArgumentsSource(AudioStringArgProvider.Valid.WaveNoteAccidentalAndOctave.class)
+    void pitchConstructorWithNoteNonNullAccidentalAndNonNegativeOctaveDoesNotThrow(String audioString,
+            Audio audio) {
+        final Optional<Audio> parsedAudio = AudioString.parse(audioString);
+        assertTrue(parsedAudio.isPresent());
+        assertEquals(audio, parsedAudio.get());
     }
 }
