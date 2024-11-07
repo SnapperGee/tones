c23174c1a1dd3e11a04722cbe238a67a8ada1488
Snap <XxSnapperGeexX@gmail.com>
2024-11-06 22:04:04 -0800

    Reduce indentation.

--- app/src/main/java/sogott/beep/CliOption.java
+++ app/src/main/java/sogott/beep/CliOption.java
@@ -5,64 +5,64 @@ import java.util.NoSuchElementException;
 import org.apache.commons.cli.Option;
 
 enum CliOption {
-        BPM(Option.builder("b")
-                        .argName("INTEGER")
-                        .longOpt("bpm")
-                        .hasArg()
-                        .desc("Set speed of playback to INTEGER beats per minute (defaults to 140).")
-                        .converter(Integer::parseInt)
-                        .build()),
-        NOTE_BEAT_VALUE(Option.builder("n")
-                        .argName("INTEGER")
-                        .longOpt("note-beat-value")
-                        .hasArg()
-                        .desc("Set the note value of a beat (defaults to 4).")
-                        .build()),
-        WAVE(Option.builder("w")
-                        .argName("WAVE")
-                        .longOpt("wave")
-                        .hasArg()
-                        .converter(aString -> Arrays.stream(Wave.values())
-                                        .filter(wave -> wave.stringValueAliases().stream()
-                                                        .anyMatch(waveStringValue -> aString
-                                                                        .equalsIgnoreCase(waveStringValue)))
-                                        .findFirst()
-                                        .orElseThrow(() -> new NoSuchElementException(
-                                                        "Invalid WAVE command line option argument.")))
-                        .desc("Set default wave shape to WAVE (defaults to SIN).")
-                        .build()),
-        VERSION(Option.builder("v")
-                        .longOpt("version")
-                        .desc("Prints currently installed version (ignores all other arguments except the help option).")
-                        .build()),
-        HELP(Option.builder("h")
-                        .longOpt("help")
-                        .desc("Ignores all other arguments and prints this help message.")
-                        .build());
+    BPM(Option.builder("b")
+            .argName("INTEGER")
+            .longOpt("bpm")
+            .hasArg()
+            .desc("Set speed of playback to INTEGER beats per minute (defaults to 140).")
+            .converter(Integer::parseInt)
+            .build()),
+    NOTE_BEAT_VALUE(Option.builder("n")
+            .argName("INTEGER")
+            .longOpt("note-beat-value")
+            .hasArg()
+            .desc("Set the note value of a beat (defaults to 4).")
+            .build()),
+    WAVE(Option.builder("w")
+            .argName("WAVE")
+            .longOpt("wave")
+            .hasArg()
+            .converter(aString -> Arrays.stream(Wave.values())
+                    .filter(wave -> wave.stringValueAliases().stream()
+                            .anyMatch(waveStringValue -> aString
+                                    .equalsIgnoreCase(waveStringValue)))
+                    .findFirst()
+                    .orElseThrow(() -> new NoSuchElementException(
+                            "Invalid WAVE command line option argument.")))
+            .desc("Set default wave shape to WAVE (defaults to SIN).")
+            .build()),
+    VERSION(Option.builder("v")
+            .longOpt("version")
+            .desc("Prints currently installed version (ignores all other arguments except the help option).")
+            .build()),
+    HELP(Option.builder("h")
+            .longOpt("help")
+            .desc("Ignores all other arguments and prints this help message.")
+            .build());
 
-        final static String CMD_LINE_SYNTAX = "Usage: beep [--%s|-%s %s] [--%s|-%s %s] [--%s|-%s] [--%s|-%s %s] [--%s|-%s] [WAVE>]NOTE.INTEGER..."
-                        .formatted(CliOption.BPM.value().getLongOpt(),
-                                        CliOption.BPM.value().getOpt(),
-                                        CliOption.BPM.value().getArgName(),
-                                        CliOption.NOTE_BEAT_VALUE.value().getLongOpt(),
-                                        CliOption.NOTE_BEAT_VALUE.value().getOpt(),
-                                        CliOption.NOTE_BEAT_VALUE.value().getArgName(),
-                                        CliOption.VERSION.value().getLongOpt(),
-                                        CliOption.VERSION.value().getOpt(),
-                                        CliOption.WAVE.value().getLongOpt(),
-                                        CliOption.WAVE.value().getOpt(),
-                                        CliOption.WAVE.value().getArgName(),
-                                        CliOption.HELP.value().getLongOpt(),
-                                        CliOption.HELP.value().getOpt())
-                        + "\nPlay musical note based beeps.\nExample: beep C4.4 D4.4 E-4.8 D4.8";
+    final static String CMD_LINE_SYNTAX = "Usage: beep [--%s|-%s %s] [--%s|-%s %s] [--%s|-%s] [--%s|-%s %s] [--%s|-%s] [WAVE>]NOTE.INTEGER..."
+            .formatted(CliOption.BPM.value().getLongOpt(),
+                    CliOption.BPM.value().getOpt(),
+                    CliOption.BPM.value().getArgName(),
+                    CliOption.NOTE_BEAT_VALUE.value().getLongOpt(),
+                    CliOption.NOTE_BEAT_VALUE.value().getOpt(),
+                    CliOption.NOTE_BEAT_VALUE.value().getArgName(),
+                    CliOption.VERSION.value().getLongOpt(),
+                    CliOption.VERSION.value().getOpt(),
+                    CliOption.WAVE.value().getLongOpt(),
+                    CliOption.WAVE.value().getOpt(),
+                    CliOption.WAVE.value().getArgName(),
+                    CliOption.HELP.value().getLongOpt(),
+                    CliOption.HELP.value().getOpt())
+            + "\nPlay musical note based beeps.\nExample: beep C4.4 D4.4 E-4.8 D4.8";
 
-        final private Option _value;
+    final private Option _value;
 
-        private CliOption(Option value) {
-                this._value = value;
-        }
+    private CliOption(Option value) {
+        this._value = value;
+    }
 
-        Option value() {
-                return this._value;
-        }
+    Option value() {
+        return this._value;
+    }
 }
--- app/src/main/java/sogott/beep/Main.java
+++ app/src/main/java/sogott/beep/Main.java
@@ -15,113 +15,113 @@ import org.apache.commons.cli.ParseException;
 import static java.util.Collections.unmodifiableList;
 
 final public class Main {
-        public static void main(String[] args) {
-                final Options options = new Options()
-                                .addOption(CliOption.BPM.value())
-                                .addOption(CliOption.NOTE_BEAT_VALUE.value())
-                                .addOption(CliOption.WAVE.value())
-                                .addOption(CliOption.VERSION.value())
-                                .addOption(CliOption.HELP.value());
+    public static void main(String[] args) {
+        final Options options = new Options()
+                .addOption(CliOption.BPM.value())
+                .addOption(CliOption.NOTE_BEAT_VALUE.value())
+                .addOption(CliOption.WAVE.value())
+                .addOption(CliOption.VERSION.value())
+                .addOption(CliOption.HELP.value());
 
-                final CommandLineParser cliParser = new DefaultParser(false);
+        final CommandLineParser cliParser = new DefaultParser(false);
 
-                try {
-                        final CommandLine cliArgs = cliParser.parse(options, args);
+        try {
+            final CommandLine cliArgs = cliParser.parse(options, args);
 
-                        if (cliArgs.hasOption(CliOption.HELP.value())) {
-                                final HelpFormatter helpFormatter = new HelpFormatter();
+            if (cliArgs.hasOption(CliOption.HELP.value())) {
+                final HelpFormatter helpFormatter = new HelpFormatter();
 
-                                helpFormatter.printHelp(
-                                                CliOption.CMD_LINE_SYNTAX,
-                                                options);
-                        } else if (cliArgs.hasOption(CliOption.VERSION.value())) {
-                                System.out.println("beep 0.0.1");
-                        } else {
-                                final int bpm = cliArgs.getParsedOptionValue(CliOption.BPM.value(), Default.BPM);
+                helpFormatter.printHelp(
+                        CliOption.CMD_LINE_SYNTAX,
+                        options);
+            } else if (cliArgs.hasOption(CliOption.VERSION.value())) {
+                System.out.println("beep 0.0.1");
+            } else {
+                final int bpm = cliArgs.getParsedOptionValue(CliOption.BPM.value(), Default.BPM);
 
-                                final int noteBeatValue = cliArgs.getParsedOptionValue(
-                                                CliOption.NOTE_BEAT_VALUE.value(),
-                                                Default.NOTE_BEAT_VALUE);
+                final int noteBeatValue = cliArgs.getParsedOptionValue(
+                        CliOption.NOTE_BEAT_VALUE.value(),
+                        Default.NOTE_BEAT_VALUE);
 
-                                final Wave wave = cliArgs.getParsedOptionValue(CliOption.WAVE.value(), Default.WAVE);
+                final Wave wave = cliArgs.getParsedOptionValue(CliOption.WAVE.value(), Default.WAVE);
 
-                                final double beatDuration = 60000.0 / bpm;
+                final double beatDuration = 60000.0 / bpm;
 
-                                final double wholeNoteDuration = beatDuration * noteBeatValue;
+                final double wholeNoteDuration = beatDuration * noteBeatValue;
 
-                                final List<String> operands = unmodifiableList(cliArgs.getArgList());
+                final List<String> operands = unmodifiableList(cliArgs.getArgList());
 
-                                final record ValidAndInvalidOperands(List<Audio> valid, List<String> invalid) {
-                                }
-
-                                final ValidAndInvalidOperands validAndInvalidOperands = operands.stream().reduce(
-                                                new ValidAndInvalidOperands(new ArrayList<Audio>(),
-                                                                new ArrayList<String>()),
-                                                (acc, operand) -> {
-
-                                                        AudioString.parse(operand, wave).ifPresentOrElse(
-                                                                        parsedOperand -> acc.valid().add(parsedOperand),
-                                                                        () -> acc.invalid().add(operand));
-
-                                                        return acc;
-                                                },
-                                                (operands1, operands2) -> {
-                                                        operands1.valid().addAll(operands2.valid());
-                                                        operands1.invalid().addAll(operands2.invalid());
-                                                        return operands1;
-                                                });
-
-                                if (!validAndInvalidOperands.invalid().isEmpty()) {
-                                        System.err.format("Illegal audio %s: [\"%s\"]",
-                                                        validAndInvalidOperands.invalid().size() == 1 ? "argument"
-                                                                        : "arguments",
-                                                        String.join("\", \"", validAndInvalidOperands.invalid()));
-                                        System.exit(222);
-                                }
-
-                                final byte[][] audioBuffers = validAndInvalidOperands.valid().stream()
-                                                .map(audio -> audio.wave() != null
-                                                                ? audio.wave()
-                                                                                .generate(Frequency.from(audio.pitch()),
-                                                                                                (int) Math.round(1.0
-                                                                                                                / audio.duration()
-                                                                                                                * wholeNoteDuration))
-                                                                : GenerateWave.silence(
-                                                                                (int) Math.round(1.0 / audio.duration()
-                                                                                                * wholeNoteDuration),
-                                                                                Default.SAMPLE_RATE))
-                                                .toArray(byte[][]::new);
-
-                                final AudioFormat audioFormat = new AudioFormat(Default.SAMPLE_RATE,
-                                                Default.SAMPLE_SIZE,
-                                                Default.CHANNELS, Default.SIGNED, Default.BIG_ENDIAN);
-
-                                final SourceDataLine line = AudioSystem.getSourceDataLine(audioFormat);
-
-                                line.open(audioFormat);
-                                line.start();
-
-                                for (final byte[] buffer : audioBuffers) {
-                                        final int soundBytes = ((int) (Default.SILENCE_RATIO * buffer.length))
-                                                        / audioFormat.getFrameSize()
-                                                        * audioFormat.getFrameSize();
-                                        line.write(buffer, 0, soundBytes);
-                                        final byte[] silenceBuffer = new byte[(buffer.length - soundBytes)
-                                                        / audioFormat.getFrameSize()
-                                                        * audioFormat.getFrameSize()];
-                                        line.write(silenceBuffer, 0, silenceBuffer.length);
-                                }
-
-                                line.drain();
-                                line.stop();
-                                line.close();
-                        }
-                } catch (ParseException e) {
-                        e.printStackTrace();
-                        System.err.format("\n%s", e.getMessage());
-                } catch (LineUnavailableException e) {
-                        e.printStackTrace();
-                        System.err.format("\n%s", e.getMessage());
+                final record ValidAndInvalidOperands(List<Audio> valid, List<String> invalid) {
                 }
+
+                final ValidAndInvalidOperands validAndInvalidOperands = operands.stream().reduce(
+                        new ValidAndInvalidOperands(new ArrayList<Audio>(),
+                                new ArrayList<String>()),
+                        (acc, operand) -> {
+
+                            AudioString.parse(operand, wave).ifPresentOrElse(
+                                    parsedOperand -> acc.valid().add(parsedOperand),
+                                    () -> acc.invalid().add(operand));
+
+                            return acc;
+                        },
+                        (operands1, operands2) -> {
+                            operands1.valid().addAll(operands2.valid());
+                            operands1.invalid().addAll(operands2.invalid());
+                            return operands1;
+                        });
+
+                if (!validAndInvalidOperands.invalid().isEmpty()) {
+                    System.err.format("Illegal audio %s: [\"%s\"]",
+                            validAndInvalidOperands.invalid().size() == 1 ? "argument"
+                                    : "arguments",
+                            String.join("\", \"", validAndInvalidOperands.invalid()));
+                    System.exit(222);
+                }
+
+                final byte[][] audioBuffers = validAndInvalidOperands.valid().stream()
+                        .map(audio -> audio.wave() != null
+                                ? audio.wave()
+                                        .generate(Frequency.from(audio.pitch()),
+                                                (int) Math.round(1.0
+                                                        / audio.duration()
+                                                        * wholeNoteDuration))
+                                : GenerateWave.silence(
+                                        (int) Math.round(1.0 / audio.duration()
+                                                * wholeNoteDuration),
+                                        Default.SAMPLE_RATE))
+                        .toArray(byte[][]::new);
+
+                final AudioFormat audioFormat = new AudioFormat(Default.SAMPLE_RATE,
+                        Default.SAMPLE_SIZE,
+                        Default.CHANNELS, Default.SIGNED, Default.BIG_ENDIAN);
+
+                final SourceDataLine line = AudioSystem.getSourceDataLine(audioFormat);
+
+                line.open(audioFormat);
+                line.start();
+
+                for (final byte[] buffer : audioBuffers) {
+                    final int soundBytes = ((int) (Default.SILENCE_RATIO * buffer.length))
+                            / audioFormat.getFrameSize()
+                            * audioFormat.getFrameSize();
+                    line.write(buffer, 0, soundBytes);
+                    final byte[] silenceBuffer = new byte[(buffer.length - soundBytes)
+                            / audioFormat.getFrameSize()
+                            * audioFormat.getFrameSize()];
+                    line.write(silenceBuffer, 0, silenceBuffer.length);
+                }
+
+                line.drain();
+                line.stop();
+                line.close();
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+            System.err.format("\n%s", e.getMessage());
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+            System.err.format("\n%s", e.getMessage());
         }
+    }
 }
