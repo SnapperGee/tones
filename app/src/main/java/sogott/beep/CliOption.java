b8135db93e103b474e69f597e691ccf990c330d2
Snap <XxSnapperGeexX@gmail.com>
2024-11-06 23:26:26 -0800

    Make fields private.

--- app/src/main/java/sogott/beep/CliOption.java
+++ app/src/main/java/sogott/beep/CliOption.java
@@ -47,9 +47,9 @@ enum CliOption {
             .desc("Ignores all other arguments and prints this help message.")
             .build());
 
-    final static CommandLineParser DEFAULT_PARSER = new DefaultParser(false);
+    private final static CommandLineParser DEFAULT_PARSER = new DefaultParser(false);
 
-    final static String CMD_LINE_SYNTAX = "Usage: beep [--%s|-%s %s] [--%s|-%s %s] [--%s|-%s] [--%s|-%s %s] [--%s|-%s] [WAVE>]NOTE.INTEGER..."
+    private final static String CMD_LINE_SYNTAX = "Usage: beep [--%s|-%s %s] [--%s|-%s %s] [--%s|-%s] [--%s|-%s %s] [--%s|-%s] [WAVE>]NOTE.INTEGER..."
             .formatted(CliOption.BPM.value().getLongOpt(),
                     CliOption.BPM.value().getOpt(),
                     CliOption.BPM.value().getArgName(),
@@ -65,7 +65,7 @@ enum CliOption {
                     CliOption.HELP.value().getOpt())
             + "\nPlay musical note based beeps.\nExample: beep C4.4 D4.4 E-4.8 D4.8";
 
-    final static Options ALL_OPTIONS = stream(CliOption.values()).reduce(
+    private final static Options ALL_OPTIONS = stream(CliOption.values()).reduce(
             new Options(),
             (options, option) -> options.addOption(option.value()),
             (options1, options2) -> options1.addOptions(options2));
