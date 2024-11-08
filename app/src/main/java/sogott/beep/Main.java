140c80d2a65dba8a4971ee7d8ce5f804b59e8136
Snap <XxSnapperGeexX@gmail.com>
2024-11-07 19:30:20 -0800

    Init AudioCollection and move audio byte buffer and playback logic to it.

--- app/src/main/java/sogott/beep/Main.java
+++ app/src/main/java/sogott/beep/Main.java
@@ -2,10 +2,6 @@ package sogott.beep;
 
 import java.util.List;
 import java.util.ArrayList;
-import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.SourceDataLine;
-import javax.sound.sampled.LineUnavailableException;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.ParseException;
 import static java.util.Collections.unmodifiableList;
@@ -63,47 +59,11 @@ final public class Main {
                     System.exit(222);
                 }
 
-                final byte[][] audioBuffers = validAndInvalidOperands.valid().stream()
-                        .map(audio -> audio.wave() != null
-                                ? audio.wave()
-                                        .generate(Frequency.from(audio.pitch()),
-                                                (int) Math.round(1.0
-                                                        / audio.duration()
-                                                        * wholeNoteDuration))
-                                : GenerateAudioByteBuffer.silence(
-                                        (int) Math.round(1.0 / audio.duration()
-                                                * wholeNoteDuration),
-                                        Default.SAMPLE_RATE))
-                        .toArray(byte[][]::new);
+                final AudioCollection audioCollection = new AudioCollection(validAndInvalidOperands.valid());
 
-                final AudioFormat audioFormat = new AudioFormat(Default.SAMPLE_RATE,
-                        Default.SAMPLE_SIZE,
-                        Default.CHANNELS, Default.SIGNED, Default.BIG_ENDIAN);
-
-                final SourceDataLine line = AudioSystem.getSourceDataLine(audioFormat);
-
-                line.open(audioFormat);
-                line.start();
-
-                for (final byte[] buffer : audioBuffers) {
-                    final int soundBytes = ((int) (Default.SILENCE_RATIO * buffer.length))
-                            / audioFormat.getFrameSize()
-                            * audioFormat.getFrameSize();
-                    line.write(buffer, 0, soundBytes);
-                    final byte[] silenceBuffer = new byte[(buffer.length - soundBytes)
-                            / audioFormat.getFrameSize()
-                            * audioFormat.getFrameSize()];
-                    line.write(silenceBuffer, 0, silenceBuffer.length);
-                }
-
-                line.drain();
-                line.stop();
-                line.close();
+                audioCollection.outputToAudio(wholeNoteDuration);
             }
         } catch (ParseException e) {
-            e.printStackTrace();
-            System.err.format("\n%s", e.getMessage());
-        } catch (LineUnavailableException e) {
             e.printStackTrace();
             System.err.format("\n%s", e.getMessage());
         }
