50f59e112ede10ffb3cb8841d1850b483985ab62
Snap <XxSnapperGeexX@gmail.com>
2024-11-09 18:46:12 -0800

    Add doc comments to Accidental enum.

--- app/src/main/java/sogott/beep/Accidental.java
+++ app/src/main/java/sogott/beep/Accidental.java
@@ -2,6 +2,25 @@ package sogott.beep;
 
 import java.util.Optional;
 
+/**
+ * Enum of values to represent musical <strong><em>accidental</em></strong>
+ * characters, the plus ({@code '+'}) character for a <i>sharp</i> &sharp; and
+ * the minus ({@code '-'}) character for a flat &flat;, and static methods for
+ * processing characters in ways related to these accidental enum values.
+ *
+ * <p>
+ * Each accidental value consists of an {@link #offset() offset} {@code int}
+ * value used by the static methods of the {@link Frequency} class to convert it
+ * into a frequency.
+ *
+ * <p>
+ * Its {@link #charValue() charValue} is also used for parsing {@code String}s
+ * to a {@link Pitch} object.
+ *
+ * @see AudioString
+ * @see Frequency
+ * @see Pitch
+ */
 enum Accidental {
     SHARP('+', 1),
     FLAT('-', -1);
