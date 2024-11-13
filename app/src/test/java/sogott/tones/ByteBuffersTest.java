8bfe5dc362d359285e15b3b4cdc3fbeb074daa36
Snap <XxSnapperGeexX@gmail.com>
2024-11-12 23:48:59 -0800

    Begin adding tests for ByteBuffers.

---
+++ app/src/test/java/sogott/tones/ByteBuffersTest.java
@@ -0,0 +1,41 @@
+package sogott.tones;
+
+import java.util.random.RandomGenerator;
+import java.util.stream.Stream;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import static java.util.Arrays.asList;
+import static java.util.Collections.unmodifiableList;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+final class ByteBuffersArgProvider {
+    final static RandomGenerator random = RandomGenerator.getDefault();
+    private final static List<Note> notes = unmodifiableList(asList(Note.values()));
+    private final static List<Accidental> accidentals = unmodifiableList(asList(Accidental.values()));
+    private final static List<Wave> waves = unmodifiableList(asList(Wave.values()));
+
+    final static List<Audio> audioObjects = notes.stream().flatMap(note -> {
+        final Pitch naturalPitch = new Pitch(note, null, random.nextInt(0, 13));
+        return accidentals.stream().flatMap(accidental -> {
+            final Pitch accidentalPitch = new Pitch(note, accidental, random.nextInt(0, 13));
+            return waves.stream()
+                    .flatMap(wave -> Stream.of(new Audio(wave, naturalPitch, 5), new Audio(wave, accidentalPitch, 5)));
+        });
+    }).toList();
+}
+
+final class ByteBuffersTest {
+
+    ///////////////////////
+    // valid constructor //
+    ///////////////////////
+
+    @Test
+    @DisplayName("new ByteBuffers(Collection<Audio>) does not throw")
+    void byteBuffersConstructorPassedValidArgumentsDoesNotThrow() {
+        assertDoesNotThrow(() -> new ByteBuffers(ByteBuffersArgProvider.audioObjects,
+                ByteBuffersArgProvider.random.nextDouble(1, 200)));
+    }
+}
