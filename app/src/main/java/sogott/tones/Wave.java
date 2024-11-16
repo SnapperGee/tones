664c4f7cd2d29a59ad054a1c1bf4df4bedeb3b15
Snap <XxSnapperGeexX@gmail.com>
2024-11-16 03:37:39 -0800

    Revert "Remove triangle wave redundant alias."
    This reverts commit 6eec793ca6939ecd24f8138960a68beacede3b9d.

--- app/src/main/java/sogott/tones/Wave.java
+++ app/src/main/java/sogott/tones/Wave.java
@@ -36,7 +36,7 @@ import static java.util.Collections.unmodifiableSet;
  */
 enum Wave {
     /**
-     * A <i>SINE</i> wave.
+     * A <i>SIN</i> wave.
      */
     SINE(GenerateWaveByteBuffer::sine, "SIN"),
 
@@ -48,7 +48,7 @@ enum Wave {
     /**
      * A <i>TRIANGLE</i> wave.
      */
-    TRIANGLE(GenerateWaveByteBuffer::triangle, "TRI"),
+    TRIANGLE(GenerateWaveByteBuffer::triangle, "TRI", Set.of("TRIANGLE")),
 
     /**
      * A <i>SAW UP</i> wave.
