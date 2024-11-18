cb2f69ac0a17411510b3e3adc7d95987b495d1ae
Snap <XxSnapperGeexX@gmail.com>
2024-11-17 19:26:01 -0800

    Fixup argument provider.

--- app/src/test/java/sogott/tones/ScaleTest.java
+++ app/src/test/java/sogott/tones/ScaleTest.java
@@ -3,90 +3,71 @@ package sogott.tones;
 import java.util.Optional;
 import java.util.stream.Stream;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.platform.suite.api.Suite;
-import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItem;
 
 final class ScaleArgProvider {
     static final class Major {
-        static final class Natural {
-            static Stream<Arguments> A() {
+        static final class Natural implements ArgumentsProvider {
+            @Override
+            public Stream<Arguments> provideArguments(ExtensionContext context) throws Exception {
                 return Stream.of(
-                        arguments(new PitchWithoutOctave(Note.A, Optional.empty())),
-                        arguments(new PitchWithoutOctave(Note.B, Optional.empty())),
-                        arguments(new PitchWithoutOctave(Note.C, Accidental.SHARP)),
-                        arguments(new PitchWithoutOctave(Note.D, Optional.empty())),
-                        arguments(new PitchWithoutOctave(Note.E, Optional.empty())),
-                        arguments(new PitchWithoutOctave(Note.F, Accidental.SHARP)),
-                        arguments(new PitchWithoutOctave(Note.G, Accidental.SHARP)));
-            }
-
-            static Stream<Arguments> B() {
-                return Stream.of(
-                        arguments(new PitchWithoutOctave(Note.B, Optional.empty())),
-                        arguments(new PitchWithoutOctave(Note.C, Accidental.SHARP)),
-                        arguments(new PitchWithoutOctave(Note.D, Accidental.SHARP)),
-                        arguments(new PitchWithoutOctave(Note.E, Accidental.SHARP)),
-                        arguments(new PitchWithoutOctave(Note.F, Accidental.SHARP)),
-                        arguments(new PitchWithoutOctave(Note.G, Accidental.SHARP)),
-                        arguments(new PitchWithoutOctave(Note.A, Accidental.SHARP)));
-            }
-
-            static Stream<Arguments> C() {
-                return Stream.of(
-                        arguments(new PitchWithoutOctave(Note.C, Optional.empty())),
-                        arguments(new PitchWithoutOctave(Note.D, Optional.empty())),
-                        arguments(new PitchWithoutOctave(Note.E, Optional.empty())),
-                        arguments(new PitchWithoutOctave(Note.F, Optional.empty())),
-                        arguments(new PitchWithoutOctave(Note.G, Optional.empty())),
-                        arguments(new PitchWithoutOctave(Note.A, Optional.empty())),
-                        arguments(new PitchWithoutOctave(Note.B, Optional.empty())));
-            }
-
-            static Stream<Arguments> D() {
-                return Stream.of(
-                        arguments(new PitchWithoutOctave(Note.D, Optional.empty())),
-                        arguments(new PitchWithoutOctave(Note.E, Optional.empty())),
-                        arguments(new PitchWithoutOctave(Note.F, Accidental.SHARP)),
-                        arguments(new PitchWithoutOctave(Note.G, Optional.empty())),
-                        arguments(new PitchWithoutOctave(Note.A, Optional.empty())),
-                        arguments(new PitchWithoutOctave(Note.B, Optional.empty())),
-                        arguments(new PitchWithoutOctave(Note.C, Accidental.SHARP)));
-            }
-
-            static Stream<Arguments> E() {
-                return Stream.of(
-                        arguments(new PitchWithoutOctave(Note.E, Optional.empty())),
-                        arguments(new PitchWithoutOctave(Note.F, Accidental.SHARP)),
-                        arguments(new PitchWithoutOctave(Note.G, Accidental.SHARP)),
-                        arguments(new PitchWithoutOctave(Note.A, Optional.empty())),
-                        arguments(new PitchWithoutOctave(Note.B, Optional.empty())),
-                        arguments(new PitchWithoutOctave(Note.C, Accidental.SHARP)),
-                        arguments(new PitchWithoutOctave(Note.D, Accidental.SHARP)));
-            }
-
-            static Stream<Arguments> F() {
-                return Stream.of(
-                        arguments(new PitchWithoutOctave(Note.F, Accidental.SHARP)),
-                        arguments(new PitchWithoutOctave(Note.G, Optional.empty())),
-                        arguments(new PitchWithoutOctave(Note.A, Optional.empty())),
-                        arguments(new PitchWithoutOctave(Note.B, Optional.empty())),
-                        arguments(new PitchWithoutOctave(Note.C, Accidental.SHARP)),
-                        arguments(new PitchWithoutOctave(Note.D, Optional.empty())),
-                        arguments(new PitchWithoutOctave(Note.E, Accidental.SHARP)));
-            }
-
-            static Stream<Arguments> G() {
-                return Stream.of(
-                        arguments(new PitchWithoutOctave(Note.G, Optional.empty())),
-                        arguments(new PitchWithoutOctave(Note.A, Optional.empty())),
-                        arguments(new PitchWithoutOctave(Note.B, Optional.empty())),
-                        arguments(new PitchWithoutOctave(Note.C, Optional.empty())),
-                        arguments(new PitchWithoutOctave(Note.D, Optional.empty())),
-                        arguments(new PitchWithoutOctave(Note.E, Optional.empty())),
-                        arguments(new PitchWithoutOctave(Note.F, Accidental.SHARP)));
+                        arguments(new PitchWithoutOctave(Note.A, Optional.empty()), Scale.Major.Natural.A),
+                        arguments(new PitchWithoutOctave(Note.B, Optional.empty()), Scale.Major.Natural.A),
+                        arguments(new PitchWithoutOctave(Note.C, Accidental.SHARP), Scale.Major.Natural.A),
+                        arguments(new PitchWithoutOctave(Note.D, Optional.empty()), Scale.Major.Natural.A),
+                        arguments(new PitchWithoutOctave(Note.E, Optional.empty()), Scale.Major.Natural.A),
+                        arguments(new PitchWithoutOctave(Note.F, Accidental.SHARP), Scale.Major.Natural.A),
+                        arguments(new PitchWithoutOctave(Note.G, Accidental.SHARP), Scale.Major.Natural.A),
+                        arguments(new PitchWithoutOctave(Note.B, Optional.empty()), Scale.Major.Natural.B),
+                        arguments(new PitchWithoutOctave(Note.C, Accidental.SHARP), Scale.Major.Natural.B),
+                        arguments(new PitchWithoutOctave(Note.D, Accidental.SHARP), Scale.Major.Natural.B),
+                        arguments(new PitchWithoutOctave(Note.E, Accidental.SHARP), Scale.Major.Natural.B),
+                        arguments(new PitchWithoutOctave(Note.F, Accidental.SHARP), Scale.Major.Natural.B),
+                        arguments(new PitchWithoutOctave(Note.G, Accidental.SHARP), Scale.Major.Natural.B),
+                        arguments(new PitchWithoutOctave(Note.A, Accidental.SHARP), Scale.Major.Natural.B),
+                        arguments(new PitchWithoutOctave(Note.C, Optional.empty()), Scale.Major.Natural.C),
+                        arguments(new PitchWithoutOctave(Note.D, Optional.empty()), Scale.Major.Natural.C),
+                        arguments(new PitchWithoutOctave(Note.E, Optional.empty()), Scale.Major.Natural.C),
+                        arguments(new PitchWithoutOctave(Note.F, Optional.empty()), Scale.Major.Natural.C),
+                        arguments(new PitchWithoutOctave(Note.G, Optional.empty()), Scale.Major.Natural.C),
+                        arguments(new PitchWithoutOctave(Note.A, Optional.empty()), Scale.Major.Natural.C),
+                        arguments(new PitchWithoutOctave(Note.B, Optional.empty()), Scale.Major.Natural.C),
+                        arguments(new PitchWithoutOctave(Note.D, Optional.empty()), Scale.Major.Natural.D),
+                        arguments(new PitchWithoutOctave(Note.E, Optional.empty()), Scale.Major.Natural.D),
+                        arguments(new PitchWithoutOctave(Note.F, Accidental.SHARP), Scale.Major.Natural.D),
+                        arguments(new PitchWithoutOctave(Note.G, Optional.empty()), Scale.Major.Natural.D),
+                        arguments(new PitchWithoutOctave(Note.A, Optional.empty()), Scale.Major.Natural.D),
+                        arguments(new PitchWithoutOctave(Note.B, Optional.empty()), Scale.Major.Natural.D),
+                        arguments(new PitchWithoutOctave(Note.C, Accidental.SHARP), Scale.Major.Natural.D),
+                        arguments(new PitchWithoutOctave(Note.E, Optional.empty()), Scale.Major.Natural.E),
+                        arguments(new PitchWithoutOctave(Note.F, Accidental.SHARP), Scale.Major.Natural.E),
+                        arguments(new PitchWithoutOctave(Note.G, Accidental.SHARP), Scale.Major.Natural.E),
+                        arguments(new PitchWithoutOctave(Note.A, Optional.empty()), Scale.Major.Natural.E),
+                        arguments(new PitchWithoutOctave(Note.B, Optional.empty()), Scale.Major.Natural.E),
+                        arguments(new PitchWithoutOctave(Note.C, Accidental.SHARP), Scale.Major.Natural.E),
+                        arguments(new PitchWithoutOctave(Note.D, Accidental.SHARP), Scale.Major.Natural.E),
+                        arguments(new PitchWithoutOctave(Note.F, Accidental.SHARP), Scale.Major.Natural.F),
+                        arguments(new PitchWithoutOctave(Note.G, Optional.empty()), Scale.Major.Natural.F),
+                        arguments(new PitchWithoutOctave(Note.A, Optional.empty()), Scale.Major.Natural.F),
+                        arguments(new PitchWithoutOctave(Note.B, Optional.empty()), Scale.Major.Natural.F),
+                        arguments(new PitchWithoutOctave(Note.C, Accidental.SHARP), Scale.Major.Natural.F),
+                        arguments(new PitchWithoutOctave(Note.D, Optional.empty()), Scale.Major.Natural.F),
+                        arguments(new PitchWithoutOctave(Note.E, Accidental.SHARP), Scale.Major.Natural.F),
+                        arguments(new PitchWithoutOctave(Note.G, Optional.empty()), Scale.Major.Natural.G),
+                        arguments(new PitchWithoutOctave(Note.A, Optional.empty()), Scale.Major.Natural.G),
+                        arguments(new PitchWithoutOctave(Note.B, Optional.empty()), Scale.Major.Natural.G),
+                        arguments(new PitchWithoutOctave(Note.C, Optional.empty()), Scale.Major.Natural.G),
+                        arguments(new PitchWithoutOctave(Note.D, Optional.empty()), Scale.Major.Natural.G),
+                        arguments(new PitchWithoutOctave(Note.E, Optional.empty()), Scale.Major.Natural.G),
+                        arguments(new PitchWithoutOctave(Note.F, Accidental.SHARP), Scale.Major.Natural.G));
             }
         }
     }
@@ -98,9 +79,11 @@ final class ScaleTest {
 
         @Suite
         final static class Natural {
-            @Test
-            void scaleMajorNaturalContainsCorrectNotes() {
-                assertTrue(true);
+            @ParameterizedTest(name = "Scale.Major.Natural.{1} contains {0}")
+            @ArgumentsSource(ScaleArgProvider.Major.Natural.class)
+            void scaleMajorNaturalCorrectNotes(PitchWithoutOctave note,
+                    Scale.Major.Natural naturalScale) {
+                assertThat(naturalScale.notes(), hasItem(note));
             }
         }
     }
