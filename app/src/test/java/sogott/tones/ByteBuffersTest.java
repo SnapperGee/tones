7007c76d4b2c69b08fad7f761c4bbf959a401fc5
Snap <XxSnapperGeexX@gmail.com>
2024-11-19 23:30:02 -0800

    Update ByteBuffersTest to account for Accidental.NATURAL.

--- app/src/test/java/sogott/tones/ByteBuffersTest.java
+++ app/src/test/java/sogott/tones/ByteBuffersTest.java
@@ -1,7 +1,6 @@
 package sogott.tones;
 
 import java.util.random.RandomGenerator;
-import java.util.stream.Stream;
 import java.util.List;
 import javax.sound.sampled.AudioFormat;
 import org.junit.jupiter.api.Test;
@@ -15,16 +14,15 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 final class ByteBuffersArgProvider {
     final static RandomGenerator random = RandomGenerator.getDefault();
-    private final static List<PitchClass> notes = unmodifiableList(asList(PitchClass.values()));
+    private final static List<PitchClass> pitchClasses = unmodifiableList(asList(PitchClass.values()));
     private final static List<Accidental> accidentals = unmodifiableList(asList(Accidental.values()));
     private final static List<Wave> waves = unmodifiableList(asList(Wave.values()));
 
-    final static List<Note> audioObjects = notes.stream().flatMap(note -> {
-        final Pitch naturalPitch = new Pitch(note, null, random.nextInt(0, 13));
+    final static List<Note> notes = pitchClasses.stream().flatMap(pitchClass -> {
         return accidentals.stream().flatMap(accidental -> {
-            final Pitch accidentalPitch = new Pitch(note, accidental, random.nextInt(0, 13));
+            final Pitch accidentalPitch = new Pitch(pitchClass, accidental, random.nextInt(0, 13));
             return waves.stream()
-                    .flatMap(wave -> Stream.of(new Note(wave, naturalPitch, 5), new Note(wave, accidentalPitch, 5)));
+                    .map(wave -> new Note(wave, accidentalPitch, random.nextInt(1, 1000)));
         });
     }).toList();
 }
@@ -34,7 +32,7 @@ final class ByteBuffersTest {
     @Test
     @DisplayName("new ByteBuffers(Collection<Audio>, # > 0) does not throw")
     void byteBuffersConstructorPassedValidArgumentsDoesNotThrow() {
-        assertDoesNotThrow(() -> new ByteBuffers(ByteBuffersArgProvider.audioObjects,
+        assertDoesNotThrow(() -> new ByteBuffers(ByteBuffersArgProvider.notes,
                 ByteBuffersArgProvider.random.nextDouble(1, 200)));
     }
 
