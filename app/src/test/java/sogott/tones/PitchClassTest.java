63a35d15ceefaa7a97e0e486f743e7dd90912c77
Snap <XxSnapperGeexX@gmail.com>
2024-11-21 20:39:22 -0800

    Add test for lowercase strings to PitchClass tests.

--- app/src/test/java/sogott/tones/PitchClassTest.java
+++ app/src/test/java/sogott/tones/PitchClassTest.java
@@ -17,8 +17,8 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 final class PitchClassTestArgsProvider {
-    private final static List<PitchLetter> pitchLetters = unmodifiableList(asList(PitchLetter.values()));
-    private final static List<Accidental> accidentals = unmodifiableList(asList(Accidental.values()));
+    final static List<PitchLetter> pitchLetters = unmodifiableList(asList(PitchLetter.values()));
+    final static List<Accidental> accidentals = unmodifiableList(asList(Accidental.values()));
 
     final static RandomGenerator random = RandomGenerator.getDefault();
 
@@ -38,6 +38,28 @@ final class PitchClassTestArgsProvider {
         final static class SoloUpperCasePitchLetterStringAndObject implements ArgumentsProvider {
             @Override
             public Stream<Arguments> provideArguments(ExtensionContext context) {
+                return pitchLetters.stream()
+                        .map(pitchLetter -> arguments(
+                                Character.toString(Character.toLowerCase(pitchLetter.charValue())),
+                                new PitchClass(pitchLetter, Accidental.NATURAL)));
+            }
+        }
+
+        final static class QualifiedLowerCasePitchClassStringAndObject implements ArgumentsProvider {
+            @Override
+            public Stream<Arguments> provideArguments(ExtensionContext context) {
+                return pitchLetters.stream()
+                        .flatMap(pitchLetter -> accidentals.stream()
+                                .map(accidental -> arguments(
+                                        new StringBuilder(2).append(Character.toLowerCase(pitchLetter.charValue()))
+                                                .append(accidental.charValue()).toString(),
+                                        new PitchClass(pitchLetter, accidental))));
+            }
+        }
+
+        final static class SoloLowerCasePitchLetterStringAndObject implements ArgumentsProvider {
+            @Override
+            public Stream<Arguments> provideArguments(ExtensionContext context) {
                 return pitchLetters.stream().map(pitchLetter -> arguments(Character.toString(pitchLetter.charValue()),
                         new PitchClass(pitchLetter, Accidental.NATURAL)));
             }
@@ -48,23 +70,45 @@ final class PitchClassTestArgsProvider {
 final class PitchClassTest {
     @ParameterizedTest(name = "PitchClass.parse(\"{0}\") returns Optional<{1}>")
     @ArgumentsSource(PitchClassTestArgsProvider.Valid.QualifiedUpperCasePitchClassStringAndObject.class)
-    void pitchClassParseStringOfQualifiedValidLetterReturnsPresentOptional(
-            String pitchLetterString,
+    void pitchClassParseStringOfQualifiedUpperCaseValidLetterReturnsPresentOptional(
+            String upperCasePitchLetterString,
             PitchClass expectedPitchClass) {
-        final Optional<PitchClass> pitchClassOptional = PitchClass.parse(pitchLetterString);
+        final Optional<PitchClass> pitchClassOptional = PitchClass.parse(upperCasePitchLetterString);
         assertTrue(pitchClassOptional.isPresent(),
-                () -> "PitchClass.parse(\"%s\") returns non present Optional".formatted(pitchLetterString));
+                () -> "PitchClass.parse(\"%s\") returns non present Optional".formatted(upperCasePitchLetterString));
         assertEquals(expectedPitchClass, pitchClassOptional.get());
     }
 
     @ParameterizedTest(name = "PitchClass.parse(\"{0}\") returns Optional<{1}>")
     @ArgumentsSource(PitchClassTestArgsProvider.Valid.SoloUpperCasePitchLetterStringAndObject.class)
-    void pitchClassParseStringOfSoloValidLetterReturnsPresentOptional(
-            String soloPitchLetterString,
+    void pitchClassParseStringOfSoloValidUpperCaseLetterReturnsPresentOptional(
+            String soloUpperCasePitchLetterString,
             PitchClass expectedPitchClass) {
-        final Optional<PitchClass> pitchClassOptional = PitchClass.parse(soloPitchLetterString);
+        final Optional<PitchClass> pitchClassOptional = PitchClass.parse(soloUpperCasePitchLetterString);
         assertTrue(pitchClassOptional.isPresent(),
-                () -> "PitchClass.parse(\"%s\") returns non present Optional".formatted(soloPitchLetterString));
+                () -> "PitchClass.parse(\"%s\") returns non present Optional".formatted(soloUpperCasePitchLetterString));
+        assertEquals(expectedPitchClass, pitchClassOptional.get());
+    }
+
+    @ParameterizedTest(name = "PitchClass.parse(\"{0}\") returns Optional<{1}>")
+    @ArgumentsSource(PitchClassTestArgsProvider.Valid.QualifiedLowerCasePitchClassStringAndObject.class)
+    void pitchClassParseStringOfQualifiedLowerCaseValidLetterReturnsPresentOptional(
+            String lowerCasePitchLetterString,
+            PitchClass expectedPitchClass) {
+        final Optional<PitchClass> pitchClassOptional = PitchClass.parse(lowerCasePitchLetterString);
+        assertTrue(pitchClassOptional.isPresent(),
+                () -> "PitchClass.parse(\"%s\") returns non present Optional".formatted(lowerCasePitchLetterString));
+        assertEquals(expectedPitchClass, pitchClassOptional.get());
+    }
+
+    @ParameterizedTest(name = "PitchClass.parse(\"{0}\") returns Optional<{1}>")
+    @ArgumentsSource(PitchClassTestArgsProvider.Valid.SoloLowerCasePitchLetterStringAndObject.class)
+    void pitchClassParseStringOfSoloValidLowerCaseLetterReturnsPresentOptional(
+            String lowerCaseSoloPitchLetterString,
+            PitchClass expectedPitchClass) {
+        final Optional<PitchClass> pitchClassOptional = PitchClass.parse(lowerCaseSoloPitchLetterString);
+        assertTrue(pitchClassOptional.isPresent(),
+                () -> "PitchClass.parse(\"%s\") returns non present Optional".formatted(lowerCaseSoloPitchLetterString));
         assertEquals(expectedPitchClass, pitchClassOptional.get());
     }
 }
