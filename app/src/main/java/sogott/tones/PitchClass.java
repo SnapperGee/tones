ac636e6c0d7766c1523f4e03e37b0b4ad7d4590a
Snap <XxSnapperGeexX@gmail.com>
2024-11-23 14:31:25 -0800

    Revert PitchClass from pojo to record.

--- app/src/main/java/sogott/tones/PitchClass.java
+++ app/src/main/java/sogott/tones/PitchClass.java
@@ -2,16 +2,8 @@ package sogott.tones;
 
 import java.util.Optional;
 
-import static java.util.Objects.hash;
-
-final class PitchClass {
-    private final PitchLetter _letter;
-    private final Accidental _accidental;
-    private final int _hashCode;
-    private final String _stringValue;
-    private final String _toString;
-
-    PitchClass(PitchLetter letter, Accidental accidental) {
+final record PitchClass(PitchLetter letter, Accidental accidental) {
+    PitchClass {
         if (letter == null) {
             throw new IllegalArgumentException("Null " + PitchLetter.class.getSimpleName());
         }
@@ -19,18 +11,6 @@ final class PitchClass {
         if (accidental == null) {
             throw new IllegalArgumentException("Null " + Accidental.class.getSimpleName());
         }
-
-        this._letter = letter;
-        this._accidental = accidental;
-        this._hashCode = hash(this._letter, this._accidental);
-        this._stringValue = this._accidental == Accidental.NATURAL
-                ? Character.toString(this._letter.charValue())
-                : new StringBuilder(2)
-                        .append(this._letter.charValue())
-                        .append(this._accidental.charValue())
-                        .toString();
-        this._toString = "%s{letter=%s, accidental=%s}".formatted(PitchClass.class.getSimpleName(), this._letter.name(),
-                this._accidental.name());
     }
 
     PitchClass(PitchLetter letter) {
@@ -38,11 +18,16 @@ final class PitchClass {
     }
 
     Pitch toPitch(int octave) {
-        return new Pitch(this._letter, this._accidental, octave);
+        return new Pitch(this.letter, this.accidental, octave);
     }
 
     String stringValue() {
-        return this._stringValue;
+        return this.accidental == Accidental.NATURAL
+                ? Character.toString(this.letter.charValue())
+                : new StringBuilder(2)
+                        .append(this.letter.charValue())
+                        .append(this.accidental.charValue())
+                        .toString();
     }
 
     static Optional<PitchClass> parse(String aString) {
@@ -63,22 +48,5 @@ final class PitchClass {
         }
 
         return Optional.empty();
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        return obj instanceof PitchClass other
-                && this._letter == other._letter
-                && this._accidental == other._accidental;
-    }
-
-    @Override
-    public int hashCode() {
-        return this._hashCode;
-    }
-
-    @Override
-    public String toString() {
-        return this._toString;
     }
 }
