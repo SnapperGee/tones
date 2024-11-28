3085973f450aaa810e57d99e77175b09c502e1c9
Snap <XxSnapperGeexX@gmail.com>
2024-11-27 19:32:03 -0800

    Remmove unused imports.

--- app/src/test/java/sogott/tones/ScaleTest.java
+++ app/src/test/java/sogott/tones/ScaleTest.java
@@ -1,6 +1,5 @@
 package sogott.tones;
 
-import java.util.Optional;
 import java.util.List;
 import java.util.stream.Stream;
 import org.junit.jupiter.params.provider.Arguments;
@@ -10,7 +9,6 @@ import org.junit.jupiter.params.provider.ArgumentsSource;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.platform.suite.api.Suite;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
