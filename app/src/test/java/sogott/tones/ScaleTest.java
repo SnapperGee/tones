02c6120dc6bf5726eb93bb19fc06e5521fd76b65
Snap <XxSnapperGeexX@gmail.com>
2024-11-28 17:38:52 -0800

    Reorder imports.

--- app/src/test/java/sogott/tones/ScaleTest.java
+++ app/src/test/java/sogott/tones/ScaleTest.java
@@ -5,11 +5,11 @@ import java.util.random.RandomGenerator;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ArgumentsSource;
 
