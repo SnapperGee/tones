408417fb73195d98f056c0ada82ae49b2b7c1f87
Snap <XxSnapperGeexX@gmail.com>
2024-11-29 19:26:52 -0800

    Add static parsePrefix method to PitchClass.

--- app/src/main/java/sogott/tones/PitchClass.java
+++ app/src/main/java/sogott/tones/PitchClass.java
@@ -1,5 +1,8 @@
 package sogott.tones;
 
+import java.util.Map;
+import java.util.Optional;
+
 final record PitchClass(PitchLetter letter, Accidental accidental) {
     PitchClass {
         if (letter == null) {
@@ -26,5 +29,16 @@ final record PitchClass(PitchLetter letter, Accidental accidental) {
                         .append(this.letter.charValue())
                         .append(this.accidental.charValue())
                         .toString();
+    }
+
+    static Optional<Map.Entry<PitchClass, String>> parsePrefix(String aString) {
+        if (aString.isBlank()) {
+            return Optional.empty();
+        }
+
+        return PitchLetter.fromChar(aString.charAt(0))
+            .flatMap(pitchLetter -> aString.length() > 1
+                ? Accidental.fromChar(aString.charAt(1)).map(accidental -> Map.entry(new PitchClass(pitchLetter, accidental), aString.substring(0, 2)))
+                : Optional.of(Map.entry(new PitchClass(pitchLetter), aString.substring(0, 1))));
     }
 }
