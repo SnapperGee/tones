83a8f04fb9329abe1dcdda483a87b69aaf3cbd5a
Snap <XxSnapperGeexX@gmail.com>
2024-12-02 16:41:17 -0800

    Make args provider for ScaleTest more readable.

--- app/src/test/java/sogott/tones/ScaleTest.java
+++ app/src/test/java/sogott/tones/ScaleTest.java
@@ -32,13 +32,17 @@ final class ScaleTestArgsProvider {
             .flatMap(scales -> scales.pitchLetterAccidentalMap().values().stream()
                 .flatMap(pitchLetterAccidentalMap -> pitchLetterAccidentalMap.values().stream().map(
                     pitchClasses ->
-                        IntStream.rangeClosed((pitchClasses.size() - 1) * -4, (pitchClasses.size() - 1) * 4).mapToObj(
-                                scalePitchIndex -> {
-                                    final int octave = random.nextInt(4, 12);
-                                    final int computedIndex = Math.floorMod(scalePitchIndex, pitchClasses.size());
-                                    final int computedOctave = octave + Math.floorDiv(scalePitchIndex, pitchClasses.size());
-                                    return arguments(pitchClasses, octave, scalePitchIndex, new Pitch(pitchClasses.get(computedIndex), computedOctave));
-                                })
+                    {
+                        final int octave = random.nextInt(0, 7);
+
+                        return IntStream.rangeClosed((pitchClasses.size() - 1) * -octave, (pitchClasses.size() - 1) * octave)
+                            .mapToObj(scalePitchIndex ->
+                            {
+                                final int computedIndex = Math.floorMod(scalePitchIndex, pitchClasses.size());
+                                final int computedOctave = octave + Math.floorDiv(scalePitchIndex, pitchClasses.size());
+                                return arguments(pitchClasses, octave, scalePitchIndex, new Pitch(pitchClasses.get(computedIndex), computedOctave));
+                            });
+                    }
                 ))
                 .flatMap(s -> s));
         }
