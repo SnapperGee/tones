62aeae0ac0c8986f182b8f82c0c83a68b63d0847
Snap <XxSnapperGeexX@gmail.com>
2025-05-02 01:16:17 -0700

    Update package dependencies.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,13 +8,13 @@ repositories {
 
 val mockitoAgent = configurations.create("mockitoAgent")
 
-val mockitoMavenPackageStr = "org.mockito:mockito-core:5.16.1"
+val mockitoMavenPackageStr = "org.mockito:mockito-core:5.17.0"
 
 dependencies {
     implementation("commons-cli:commons-cli:1.9.0")
     testImplementation(libs.junit.jupiter)
     testImplementation("org.hamcrest:hamcrest:3.0")
-    testImplementation("org.junit.platform:junit-platform-suite-api:1.12.1")
+    testImplementation("org.junit.platform:junit-platform-suite-api:1.12.2")
     testImplementation(mockitoMavenPackageStr)
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     mockitoAgent(mockitoMavenPackageStr) { isTransitive = false }
